2ca15df3e718d5a1688f73da9fe61eece37ccacb
ArielXT <arielzaratext@gmail.com>
2022-03-28 22:33:56 -0500

    Version de Internet

--- src/controller.js
+++ src/controller.js
@@ -1,7 +1,36 @@
+var sesiones = {
+    sesion1:{
+        id: 1,
+        nombre:"Ariel Alejandro",        
+        apellido:"Zarate Tanta",
+        email:"abc@gmail.com",
+        clave:"tecsup2021",
+        userdata:"Az"
+    },
+    sesion2:{
+        id: 2,
+        nombre:"Noe Jhon",
+        apellido:"Sierra Sotelo",
+        email:"zab@gmail.com",
+        clave:"trafalgar",        
+        userdata:"Ns"        
+    }
+}
+
+var emails = {
+    correo1:{
+        id: 1,
+        email:"abc@gmail.com"
+    },
+    correo2:{
+        id: 2,
+        email:"zab@gmail.com"
+    }
+}
+
 export const index = (req, res) => {
-    res.json({
-        data: "test success",
-    });
+    console.log("--->Lista de usuarios<--- \n" + JSON.stringify(sesiones,null,4));
+    res.end("Lista de usuarios \n" + JSON.stringify(sesiones, null, 4));
 };
 
 export const login = (req, res) => {
@@ -13,60 +42,45 @@ export const login = (req, res) => {
             clave,
         },
     });
+    console.log("---> Logueado Correctamente<--- \n" + "Email: " + email);
 };
 
-export const signup = (req, res) => {
-    const { nombre, apellido, email, clave } = req.body;
-    res.json({
-        data: {
-            type: "signup",
-            nombre,
-            apellido,
-            email,
-            clave,
-        },
-    });
+export const signup = (req, res) => {    
+    var n_sesion = req.body;
+    sesiones["sesion" + n_sesion.id] = n_sesion;
+    console.log("--> Registrado Correctamente <--- \n" + JSON.stringify(sesiones,null,4));
+    res.end("Post hecho Correctamente: \n" + JSON.stringify(n_sesion, null, 4));
+
 };
 
 export const showuser = (req, res) => {
-    res.json({
-        data: {
-            type: "showuser",
-            id: 737378625,
-            show: "Ariel Zarate",
-        },
-    });
+    var sesion = sesiones["sesion" + req.params.id];
+    console.log("--->Encuentra un usuario<--- \n" + JSON.stringify(sesion,null,4));
+    res.end("Encontrando al usuario \n" + JSON.stringify(sesion, null, 4));
 };
 
 export const reset = (req, res) => {
-    const { email, clave } = req.body;
-    res.json({
-        data: {
-            type: "reset",
-            email,
-            clave,
-
-        },
-    });
+    var email = req.body;
+    emails["correo: " + email.id] = email;
+    console.log("--> Registrado Correctamente <--- \n" + JSON.stringify(emails,null,4));
+    res.end("Post hecho Correctamente: \n" + JSON.stringify(email, null, 4));
 };
 
 export const update = (req, res) => {
-    const { id, userdata } = req.body;
-    res.json({
-        data: {
-            type: "update",
-            id,
-            userdata,
-        },
-    });
+    var id = parseInt(req.params.id);
+    var reset = req.body;
+    if(sesiones["sesion" + id] != null){
+        sesiones["sesion" + id] = reset;
+        console.log("--->Reseteo hecho, sesion<--- \n" + JSON.stringify(sesiones,null,4));
+        res.end("Reseteo hecho \n" + JSON.stringify(reset, null, 4));
+    }else{
+        res.end("No existe la sesion\n" + JSON.stringify(reset, null, 4));
+    }
 };
 
 export const deletear = (req, res) => {
-    const { id } = req.body;
-    res.json({
-        data: {
-            type: "deletear",
-            id,
-        },
-    });
+    var eliminar = sesiones["sesion" + req.params.id];
+    delete sesiones["sesion" + req.params.id];
+    console.log("--->Lista luego de la eliminacion<--- \n" + JSON.stringify(sesiones,null,4));
+    res.end("Usuario eliminado \n" + JSON.stringify(eliminar, null, 4));
 };
--- src/network.js
+++ src/network.js
@@ -5,9 +5,9 @@ const testRouter = Router();
 testRouter.route("/").get(index);
 testRouter.route("/login").post(login);
 testRouter.route("/signup").post(signup);
-testRouter.route('/showuser').get(showuser);
-testRouter.route("/reset").post(reset);
-testRouter.route("/update").put(update);
-testRouter.route("/deletear").delete(deletear);
+testRouter.route('/showuser/:id').get(showuser);
+testRouter.route("/reset/:id").post(reset);
+testRouter.route("/update/:id").put(update);
+testRouter.route("/deletear/:id").delete(deletear);
 
 export default testRouter;
